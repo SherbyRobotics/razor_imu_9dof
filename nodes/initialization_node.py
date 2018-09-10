f7d46f0f1454a2c680589cc1e3c69866ea97a525
WillTherrien <william.therrien@usherbrooke.ca>
2018-09-10 13:55:17 -0400

    Initialization node modifications

--- nodes/initialization_node.py
+++ nodes/initialization_node.py
@@ -11,6 +11,7 @@ import time
 from razor_imu_9dof.msg import ImuStates
 from sensor_msgs.msg import Imu
 from tf.transformations import quaternion_from_euler
+from tf.transformations import euler_from_quaternion
 from dynamic_reconfigure.server import Server
 from razor_imu_9dof.cfg import imuConfig
 from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
@@ -20,6 +21,7 @@ from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
 class Read_IMU:
 
   def __init__(self):
+
      self.imu_sub = rospy.Subscriber("imu",Imu, self.callback)
      self.imu_pub = rospy.Publisher("imu_msg", ImuStates, queue_size=1)
 
@@ -28,26 +30,36 @@ class Read_IMU:
      self.theta = []
      self.t_0 = rospy.get_rostime()
 
+     self.rad2degrees = 180.0/math.pi
+
   def callback(self,imu):
 
-      t_duration = imu.header.stamp - self.t_0
-      t_nsecs = int(t_duration.nsecs)
-      
-      if t_nsecs<=5000000000:
-        self.acc_x.append(imu.linear_acceleration.x)
-        self.acc_y.append(imu.linear_acceleration.y)
-        self.theta.append(imu.orientation.z)
+     quaternion = (
+       imu.orientation.x,
+       imu.orientation.y,
+       imu.orientation.z,
+       imu.orientation.w)
+     (roll,pitch,yaw) = euler_from_quaternion(quaternion)
 
-      init_acc_x = np.mean(self.acc_x)
-      init_acc_y = np.mean(self.acc_y)
-      init_theta = np.mean(self.theta)
+
+     t_duration = imu.header.stamp - self.t_0
+     t_nsecs = int(t_duration.nsecs)
+
+      
+     if t_nsecs<=5000000000:
+       self.acc_x.append(imu.linear_acceleration.x)
+       self.acc_y.append(imu.linear_acceleration.y)
+
+
+     init_acc_x = np.mean(self.acc_x)
+     init_acc_y = np.mean(self.acc_y)
         
-      msg = ImuStates()
-      msg.header.stamp = t_duration
-      msg.state_accx = (imu.linear_acceleration.x - init_acc_x)
-      msg.state_accy = (imu.linear_acceleration.y - init_acc_y)
-      msg.state_theta = (imu.orientation.z - init_theta)
-      self.imu_pub.publish(msg)
+     msg = ImuStates()
+     msg.header.stamp = t_duration
+     msg.state_accx = (imu.linear_acceleration.x - init_acc_x)
+     msg.state_accy = (imu.linear_acceleration.y - init_acc_y)
+     msg.state_theta = (yaw)
+     self.imu_pub.publish(msg)
 
 #######################################################################
       
